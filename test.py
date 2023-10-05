db607abc66122590c006a18c8fdcece5c4df52fe
Edward <emorg31@lsu.edu>
2023-10-05 08:43:40 -0500

    critic implemented

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
 from hw1 import Composer
 from midi2seq import process_midi_seq, seq2piano
+import torch
+from torch.utils.data import DataLoader, TensorDataset
 
+bsz = 0
+epoch = 0
 piano_seq = torch.from_numpy(process_midi_seq())
 loader = DataLoader(TensorDataset(piano_seq), shuffle=True, batch_size=bsz, num_workers=4)
 
-cps = Composer()
-for i in range(epoch):
-    for x in loader:
-        cps.train(x[0].cuda(0).long())
+# cps = Composer()
+# for i in range(epoch):
+#     for x in loader:
+#         cps.train(x[0].cuda(0).long())
         
-midi = cps.compose(100)
-midi = seq2piano(midi)
-midi.write('piano1.midi')
+# midi = cps.compose(100)
+# midi = seq2piano(midi)
+# midi.write('piano1.midi')
